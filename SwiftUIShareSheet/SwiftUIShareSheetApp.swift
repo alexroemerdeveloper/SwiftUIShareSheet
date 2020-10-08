38e4ee21d4742f0cf86941f806c196b39b9aab24
Alexander Römer <alex@roemer.design>
2020-10-08 09:56:10 +0200

    Initial Commit

---
+++ SwiftUIShareSheet/SwiftUIShareSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIShareSheetApp.swift
+//  SwiftUIShareSheet
+//
+//  Created by Alexander Römer on 08.10.20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIShareSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
